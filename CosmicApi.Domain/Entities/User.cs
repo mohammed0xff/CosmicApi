a4e6eae9f69661914550bea98a27085b45f6239e
mohammed0xff <mohammed0xff@gamil.com>
2023-02-23 15:18:10 +0200

    add user and roles

---
+++ CosmicApi.Domain/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿using CosmicApi.Domain.Entities.Common;
+
+
+namespace CosmicApi.Domain.Entities{
+
+    public class User : BaseEntity
+    {
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string Role { get; set; } = null!;
+    }
+}
