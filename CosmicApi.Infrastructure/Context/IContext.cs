3844a7af733dcac7087d25e605b4f6fe274d4b2b
mohammed0xff <mohammed0xff@gamil.com>
2023-02-26 23:36:27 +0200

    add `IContext`

---
+++ CosmicApi.Infrastructure/Context/IContext.cs
@@ -0,0 +1,20 @@
+﻿using CosmicApi.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace CosmicApi.Infrastructure.Context
+{
+    public interface IContext : IAsyncDisposable, IDisposable
+    {
+        public DatabaseFacade Database { get; }
+        public DbSet<Galaxy> Galaxies { get; set; }
+        public DbSet<Star> Stars { get; set; } 
+        public DbSet<Planet> Planets { get; set; }
+        public DbSet<Picture> Pictures { get; set; }
+        public DbSet<Moon> Moons { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<RefreshToken> Tokens { get; set; }
+
+        public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
