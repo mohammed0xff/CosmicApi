a357d5f2ba14107c37c20fe2d098e102398bc076
mohammed0xff <mohammed0xff@gamil.com>
2023-03-01 23:23:30 +0200

    add `AddMediatRConfig` basic mediatR configuration.

---
+++ CosmicApi/Configurations/MediatRConfig.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using CosmicApi.Application.Common.Behaviors;
+using CosmicApi.Application.Common.Handlers;
+using CosmicApi.Application.Common;
+
+namespace CosmicApi.Configurations
+{
+    public static class MediatRConfig
+    {
+        public static IServiceCollection AddMediatRConfig(this IServiceCollection services)
+        {
+            services.AddMediatR(typeof(CosmicApi.Application.IAssemblyMarker).Assembly);
+
+            return services;
+        }
+    }
+}
