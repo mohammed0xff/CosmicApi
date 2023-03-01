6a73969111643acfe5fcdcbd555affa11f5af698
mohammed0xff <mohammed0xff@gamil.com>
2023-03-01 23:20:24 +0200

    add : basic AuthenticationConfiguration.

---
+++ CosmicApi/Configurations/AuthenticationConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using CosmicApi.Infrastructure.Services.TokenService;
+
+namespace CosmicApi.Configurations
+{
+    public static class AuthenticationConfiguration
+    {
+        public static IServiceCollection ConfigureAuthentication(this IServiceCollection services)
+        {
+            services.AddTransient<ITokenService, JwtService>();
+            
+            return services;
+        }
+    }
+}
