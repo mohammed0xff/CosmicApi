cc0148e37ce1bd228ff96c45480bc739842790fd
mohammed0xff <mohammed0xff@gamil.com>
2023-03-02 23:38:53 +0200

    add feature `GetUsers` for getting paged user response

---
+++ src/CosmicApi.Application/Features/Users/GetUsers/GetUsersHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using CosmicApi.Application.Common.Responses;
+using CosmicApi.Application.Extensions;
+using CosmicApi.Infrastructure.Context;
+
+
+namespace CosmicApi.Application.Features.Users.GetUsers
+{
+    public class GetUsersHandler : IRequestHandler<GetUsersRequest, PaginatedList<UserResponse>>
+    {
+        private readonly IContext _context;
+
+        private readonly IMapper _mapper;
+
+        public GetUsersHandler(IMapper mapper, IContext context)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
+
+        public async Task<PaginatedList<UserResponse>> Handle(GetUsersRequest request, CancellationToken cancellationToken)
+        {
+            var users = _context.Users
+                .WhereIf(!string.IsNullOrEmpty(request.Email),
+                    x => EF.Functions.Like(x.Email, $"%{request.Email}%"));
+
+            return await _mapper.ProjectTo<UserResponse>(users)
+                .ToPaginatedListAsync(request.CurrentPage, request.PageSize);
+        }
+    }
+}
---
+++ src/CosmicApi.Application/Features/Users/GetUsers/GetUsersRequest.cs
@@ -0,0 +1,10 @@
+﻿using CosmicApi.Application.Common.Requests;
+using CosmicApi.Application.Common.Responses;
+using MediatR;
+
+namespace CosmicApi.Application.Features.Users.GetUsers;
+
+public record GetUsersRequest : PaginatedRequest, IRequest<PaginatedList<UserResponse>>
+{
+    public string? Email { get; init; }
+}
