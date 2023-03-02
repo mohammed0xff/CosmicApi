54270d942b027b8826e1ae41fb963dabf4fdab65
mohammed0xff <mohammed0xff@gamil.com>
2023-03-02 23:06:18 +0200

    add migrations

---
+++ src/CosmicApi.Infrastructure/Migrations/20230228164613_InitialCreate.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CosmicApi.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Galaxies",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Type = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Galaxies", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pictures",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: false),
+                    Height = table.Column<int>(type: "int", nullable: true),
+                    Width = table.Column<int>(type: "int", nullable: true),
+                    AddedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    LuminaryId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pictures", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(254)", maxLength: 254, nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BlackHole",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    GalaxyId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BlackHole", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BlackHole_Galaxies_GalaxyId",
+                        column: x => x.GalaxyId,
+                        principalTable: "Galaxies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Stars",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NumberOfPlanets = table.Column<int>(type: "int", nullable: true),
+                    GalaxyId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stars", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Stars_Galaxies_GalaxyId",
+                        column: x => x.GalaxyId,
+                        principalTable: "Galaxies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tokens",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Token = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AddedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ExpiryDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId1 = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tokens", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tokens_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Tokens_Users_UserId1",
+                        column: x => x.UserId1,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Planets",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NumberOfMoons = table.Column<int>(type: "int", nullable: false),
+                    GalaxyId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    StarId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Planets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Planets_Galaxies_GalaxyId",
+                        column: x => x.GalaxyId,
+                        principalTable: "Galaxies",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Planets_Stars_StarId",
+                        column: x => x.StarId,
+                        principalTable: "Stars",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Moons",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PlanetId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Moons", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Moons_Planets_PlanetId",
+                        column: x => x.PlanetId,
+                        principalTable: "Planets",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BlackHole_GalaxyId",
+                table: "BlackHole",
+                column: "GalaxyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Galaxies_Name",
+                table: "Galaxies",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Moons_PlanetId",
+                table: "Moons",
+                column: "PlanetId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Planets_GalaxyId",
+                table: "Planets",
+                column: "GalaxyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Planets_Name",
+                table: "Planets",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Planets_StarId",
+                table: "Planets",
+                column: "StarId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Stars_GalaxyId",
+                table: "Stars",
+                column: "GalaxyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Stars_Name",
+                table: "Stars",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tokens_UserId",
+                table: "Tokens",
+                column: "UserId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tokens_UserId1",
+                table: "Tokens",
+                column: "UserId1",
+                unique: true,
+                filter: "[UserId1] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BlackHole");
+
+            migrationBuilder.DropTable(
+                name: "Moons");
+
+            migrationBuilder.DropTable(
+                name: "Pictures");
+
+            migrationBuilder.DropTable(
+                name: "Tokens");
+
+            migrationBuilder.DropTable(
+                name: "Planets");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Stars");
+
+            migrationBuilder.DropTable(
+                name: "Galaxies");
+        }
+    }
+}
---
+++ src/CosmicApi.Infrastructure/Migrations/20230228220833_add-more-properties-to-Galaxy.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CosmicApi.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addmorepropertiestoGalaxy : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AbsoluteMagnitude",
+                table: "Galaxies",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Age",
+                table: "Galaxies",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "EscapeVelocity",
+                table: "Galaxies",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NumberOfStars",
+                table: "Galaxies",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Radius",
+                table: "Galaxies",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AbsoluteMagnitude",
+                table: "Galaxies");
+
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "Galaxies");
+
+            migrationBuilder.DropColumn(
+                name: "EscapeVelocity",
+                table: "Galaxies");
+
+            migrationBuilder.DropColumn(
+                name: "NumberOfStars",
+                table: "Galaxies");
+
+            migrationBuilder.DropColumn(
+                name: "Radius",
+                table: "Galaxies");
+        }
+    }
+}
