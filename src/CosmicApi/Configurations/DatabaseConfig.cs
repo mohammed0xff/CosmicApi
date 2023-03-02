c6bd7ce3a14f28a99f43b919375a1c6274e04e4f
mohammed0xff <mohammed0xff@gamil.com>
2023-03-02 23:25:48 +0200

    add database - before run - initiation

---
+++ src/CosmicApi/Configurations/DatabaseConfig.cs
@@ -0,0 +1,31 @@
+﻿using CosmicApi.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CosmicApi.Configurations
+{
+    public static class DatabaseConfigurationExtension
+    {
+
+        public static async Task InitDatabase(this IApplicationBuilder app)
+        {
+            await using var serviceScope = app.ApplicationServices.CreateAsyncScope();
+            await using var context = serviceScope.ServiceProvider
+                .GetRequiredService<ApplicationDbContext>();
+
+            // Ensure db created 
+            await context.Database.EnsureCreatedAsync();
+            
+            // if there are no applied migrations .. apply them all
+            if (!(await context.Database.GetAppliedMigrationsAsync()).Any())
+            {
+                await context.Database.MigrateAsync();
+            }
+            
+            // Ensure data seed 
+            await context.EnsureSeedDataAsync();
+
+            // logging that later
+            var connectionString = context.Database.GetConnectionString();
+        }
+    }
+}
