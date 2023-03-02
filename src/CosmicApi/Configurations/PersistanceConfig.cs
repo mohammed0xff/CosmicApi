8073043e0edcb3a5a7605a31eddc9a30ee443f0f
mohammed0xff <mohammed0xff@gamil.com>
2023-03-02 23:09:03 +0200

    add PersistanceConfig adding `AddDbContext` to builder and `ApplicationDbContext` as transient

---
+++ src/CosmicApi/Configurations/PersistanceConfig.cs
@@ -0,0 +1,19 @@
+﻿using CosmicApi.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CosmicApi.Api.Configurations;
+
+public static class PersistanceConfig
+{
+    public static IServiceCollection ConfigurePersistence(this IServiceCollection services, IConfiguration configuration)
+    {
+
+        services.AddDbContext<ApplicationDbContext>(o =>
+        {
+            o.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+        });
+        services.AddTransient<IContext, ApplicationDbContext>();
+
+        return services;
+    }
+}
