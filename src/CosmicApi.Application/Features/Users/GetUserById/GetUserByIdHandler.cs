4ecf72f251f100a3453df9f34e849f7ad20776c0
mohammed0xff <mohammed0xff@gamil.com>
2023-03-02 23:39:10 +0200

    add feature `GetUserById` for getting user by id

---
+++ src/CosmicApi.Application/Features/Users/GetUserById/GetUserByIdHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using CosmicApi.Infrastructure.Context;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace CosmicApi.Application.Features.Users.GetUserById
+{
+
+    public class GetUserByIdHandler : IRequestHandler<GetUserByIdRequest, UserResponse?>
+    {
+        private readonly IContext _context;
+        private readonly IMapper _mapper;
+
+        public GetUserByIdHandler(IMapper mapper, IContext context)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
+
+        public async Task<UserResponse?> Handle(GetUserByIdRequest request, CancellationToken cancellationToken)
+        {
+            var result = await _context.Users
+                .FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken);
+            if (result is null) return null;
+
+            return _mapper.Map<UserResponse>(result);
+        }
+    }
+}
---
+++ src/CosmicApi.Application/Features/Users/GetUserById/GetUserByIdRequest.cs
@@ -0,0 +1,7 @@
+﻿using CosmicApi.Domain.Entities.Common;
+using MediatR;
+
+namespace CosmicApi.Application.Features.Users.GetUserById
+{
+    public record GetUserByIdRequest(Guid Id) : IRequest<UserResponse?>;
+}
