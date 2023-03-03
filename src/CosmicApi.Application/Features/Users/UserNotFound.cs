47231464e5db7bb75fb63150d091c3fdbebeef38
mohammed0xff <mohammed0xff@gamil.com>
2023-03-03 21:20:51 +0200

    add : `UserResponse` and `UserNotFoundException`

---
+++ src/CosmicApi.Application/Features/Users/UserNotFound.cs
@@ -0,0 +1,13 @@
+﻿using CosmicApi.Application.Common.Responses;
+
+namespace CosmicApi.Application.Features.Users;
+
+public record UserNotFound : NotFound {}
+
+public class UserNotFoundException : Exception {
+	public UserNotFoundException(Guid userId) 
+		: base($"User with id '{userId}' not found.")
+	{
+	}
+
+}
---
+++ src/CosmicApi.Application/Features/Users/UserResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace CosmicApi.Application.Features.Users
+{
+    public record UserResponse
+    {
+        public Guid Id { get; init; }
+        public string Username { get; init; } = null!;
+        public string Email { get; init; } = null!;
+    }
+}
