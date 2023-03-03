377dfada0ae8a166dd4c48c68a31909c4d34afea
mohammed0xff <mohammed0xff@gamil.com>
2023-03-03 21:36:23 +0200

    add PlanetResponse as a planet DTO

---
+++ src/CosmicApi.Application/Features/Planet/PlanetResponse.cs
@@ -0,0 +1,19 @@
+﻿using CosmicApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CosmicApi.Application.Features.Planets
+{
+    public class PlanetResponse
+    {
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public int? NumberOfMoons { get; set; } = null;
+
+        public Guid GalaxyId { get; set; }
+        public Guid StarId { get; set; }
+    }
+}
