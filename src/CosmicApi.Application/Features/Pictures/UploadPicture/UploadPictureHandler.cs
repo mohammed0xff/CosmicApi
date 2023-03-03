f7d964ef794c0e16606e01fde8b302aa88f34b15
mohammed0xff <mohammed0xff@gamil.com>
2023-03-03 22:02:11 +0200

    add feature upload picture

---
+++ src/CosmicApi.Application/Features/Pictures/UploadPicture/UploadPictureHandler.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using AutoMapper;
+using CosmicApi.Infrastructure.Context;
+using CosmicApi.Infrastructure.Services;
+
+namespace CosmicApi.Application.Features.Pictures.UploadPicture
+{
+    public class UploadPictureeHandler : IRequestHandler<UploadPictureRequest, PictureResponse?>
+    {
+        private readonly IContext _context;
+        private readonly IMapper _mapper;
+        private readonly IPictureService _pictureService;
+
+        public UploadPictureeHandler(IContext context, IMapper mapper, IPictureService pictureService)
+        {
+            _context = context;
+            _mapper = mapper;
+            _pictureService = pictureService;
+        }
+        public async Task<PictureResponse?> Handle(UploadPictureRequest request, CancellationToken cancellationToken)
+        {
+            var uploadedImage = _pictureService.UploadPicture(request.FormFile);
+            if (uploadedImage == null) 
+                return null;
+            
+            uploadedImage.LuminaryId = request.LuminaryId;
+            await _context.Pictures.AddAsync(uploadedImage, cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+                
+            return _mapper.Map<PictureResponse>(uploadedImage);
+        }
+    }
+}
---
+++ src/CosmicApi.Application/Features/Pictures/UploadPicture/UploadPictureRequest.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+
+namespace CosmicApi.Application.Features.Pictures.UploadPicture
+{
+    public record UploadPictureRequest : IRequest<PictureResponse?>
+    {
+        public string? Name { get; set; }
+        public IFormFile FormFile { get; set; }
+        public Guid LuminaryId { get; set; }
+    }
+}
