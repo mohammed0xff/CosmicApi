719bc352e2d79dc712c87c568cb91582ad1908cc
mohammed0xff <mohammed0xff@gamil.com>
2023-03-03 21:51:27 +0200

    add featrue GetPictures

---
+++ src/CosmicApi.Application/Features/Pictures/GetPictures/GetPictureHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using AutoMapper;
+using CosmicApi.Application.Common.Responses;
+using CosmicApi.Infrastructure.Context;
+
+namespace CosmicApi.Application.Features.Pictures.GetPictures
+{
+    public class GetPictureeHandler : IRequestHandler<GetLuminaryPicturesRequest, PaginatedList<PictureResponse>>
+    {
+        private readonly IContext _context;
+        private readonly IMapper _mapper;
+
+        public GetPictureeHandler(IContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        public async Task<PaginatedList<PictureResponse>> Handle(GetLuminaryPicturesRequest request, CancellationToken cancellationToken)
+        {
+            var pictures = _context.Pictures
+                .Where(x => x.LuminaryId == request.LuminaryId);
+
+            if (request.New)
+                pictures.OrderBy(x => x.AddedAt);
+
+            return await _mapper.ProjectTo<PictureResponse>(pictures)
+                .ToPaginatedListAsync(request.CurrentPage, request.PageSize);
+
+        }
+    }
+}
---
+++ src/CosmicApi.Application/Features/Pictures/GetPictures/GetPicturesRequest.cs
@@ -0,0 +1,17 @@
+﻿using CosmicApi.Application.Common.Requests;
+using CosmicApi.Application.Common.Responses;
+using MediatR;
+
+namespace CosmicApi.Application.Features.Pictures.GetPictures
+{
+    public record GetPicturesRequest : PaginatedRequest, IRequest<PaginatedList<PictureResponse>>
+    {
+        public bool New { get; set; } = false;
+    }
+    public record GetLuminaryPicturesRequest : GetPicturesRequest
+    {
+        public GetLuminaryPicturesRequest(GetPicturesRequest request) : base(request) {}
+        public Guid? LuminaryId { get; set; }
+    }
+}
+
