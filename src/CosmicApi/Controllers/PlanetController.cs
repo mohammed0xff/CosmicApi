ea1acaeab8c28fab29bf834dee82309eeca5aeed
mohammed0xff <mohammed0xff@gamil.com>
2023-03-03 21:34:38 +0200

    add controller PlanetController

---
+++ src/CosmicApi/Controllers/PlanetController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MediatR;
+using CosmicApi.Application.Common.Responses;
+using CosmicApi.Application.Features.Pictures;
+using CosmicApi.Application.Features.Pictures.GetPictures;
+using CosmicApi.Application.Features.Planets;
+using CosmicApi.Application.Features.Planets.CreatePlanet;
+
+namespace CosmicApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlanetController : ControllerBase
+    {
+        public IMediator _mediator;
+        public PlanetController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<PaginatedList<PlanetResponse>>> Get([FromQuery] GetPlanetRequest request)
+        {
+            return Ok(await _mediator.Send(request));
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(PlanetResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(PlanetResponse), StatusCodes.Status404NotFound)]
+        public async Task<ActionResult> GetPlanetById([FromRoute] Guid id)
+        {
+            var result = await _mediator.Send(new GetPlanetByIdRequest(id));
+            return result == null ? NotFound() : Ok(result);
+        }
+
+        [HttpGet("{id}/pictures")]
+        [ProducesResponseType(typeof(PaginatedList<PictureResponse>), StatusCodes.Status200OK)]
+        public async Task<ActionResult> Pictures([FromRoute] Guid id, [FromQuery] GetPicturesRequest request)
+        {
+            return Ok(await _mediator.Send(new GetLuminaryPicturesRequest(request) with { LuminaryId = id }));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(PlanetResponse), StatusCodes.Status201Created)]
+        public async Task<ActionResult> Create([FromBody] CreatePlanetRequest request)
+        {
+            return CreatedAtAction(nameof(Create), await _mediator.Send(request));
+        }
+    }
+}
