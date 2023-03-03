a793214dd79ee4e5643df43f65a5de8eaf13a037
mohammed0xff <mohammed0xff@gamil.com>
2023-03-03 21:38:15 +0200

    add moon response DTO

---
+++ src/CosmicApi.Application/Features/Moons/MoonResponse.cs
@@ -0,0 +1,10 @@
+﻿using CosmicApi.Application.Common.Requests;
+namespace CosmicApi.Application.Features.Moons
+{
+    public record MoonResponse 
+    {
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public Guid PlanetId { get; set; }
+    }
+}
