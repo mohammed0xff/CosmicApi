ff4391a85b2beff25f727e9fcb195e6b874a7332
mohammed0xff <mohammed0xff@gamil.com>
2023-03-03 21:25:24 +0200

    add GalaxyResponse DTO

---
+++ src/CosmicApi.Application/Features/Galaxies/GalaxyResponse.cs
@@ -0,0 +1,18 @@
+﻿using CosmicApi.Domain.Entities.Enums;
+
+namespace CosmicApi.Application.Features.Galaxies
+{
+    public class GalaxyResponse
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public string? EscapeVelocity { get; set; }
+        public string? AbsoluteMagnitude { get; set; }
+        public string? Age { get; set; }
+        public string? Radius { get; set; }
+        public string? NumberOfStars { get; set; }
+        public string? Type { get; set; } 
+
+    }
+}
