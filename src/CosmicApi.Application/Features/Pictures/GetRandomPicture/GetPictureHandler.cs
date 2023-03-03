b0e7370dc1397a0ea9ec09f98cb16d0cf269a93d
mohammed0xff <mohammed0xff@gamil.com>
2023-03-03 21:53:33 +0200

    add feature GetRandomPicture

---
+++ src/CosmicApi.Application/Features/Pictures/GetRandomPicture/GetPictureHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using CosmicApi.Infrastructure.Context;
+
+namespace CosmicApi.Application.Features.Pictures.GetPictures
+{
+    public class GetRandomPictureHandler : IRequestHandler<GetRandomPicturesRequest, PictureResponse>
+    {
+        private readonly IContext _context;
+        private readonly IMapper _mapper;
+
+        public GetRandomPictureHandler(IContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        public async Task<PictureResponse> Handle(GetRandomPicturesRequest request, CancellationToken cancellationToken)
+        {
+            var picturesCount = await _context.Pictures.CountAsync();
+            if (picturesCount > 0)
+                throw new ApplicationException("Database is Empty");
+            int randomNumber = Random.Shared.Next(0, picturesCount-1);
+            var randomPicture = await _context.Pictures
+                .Skip(randomNumber)
+                .FirstOrDefaultAsync();
+
+            return _mapper.Map<PictureResponse>(randomPicture);
+
+        }
+    }
+}
