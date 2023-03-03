f48923b8f0fbbf187d8ada429c60fa1b6ab59420
mohammed0xff <mohammed0xff@gamil.com>
2023-03-03 21:24:41 +0200

    add feature DeleteGalaxy

---
+++ src/CosmicApi.Application/Features/Galaxies/DeleteGalaxy/DeleteGalaxyHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using CosmicApi.Infrastructure.Context;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace CosmicApi.Application.Features.Galaxies.CreateGalaxy
+{
+    public class DeleteGalaxyHandler : IRequestHandler<DeleteGalaxyRequest, bool>
+    {
+        private readonly IContext _context;
+        private readonly IMapper _mapper;
+
+        public DeleteGalaxyHandler(IContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        public async Task<bool> Handle(DeleteGalaxyRequest request, CancellationToken cancellationToken)
+        {
+            var galaxy = await _context.Galaxies
+                .FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken);
+            if (galaxy is null)
+                return false;    
+            
+            _context.Galaxies.Remove(galaxy!);
+
+            return await _context.SaveChangesAsync(cancellationToken) > 0;
+        }
+    }
+
+}
---
+++ src/CosmicApi.Application/Features/Galaxies/DeleteGalaxy/DeleteGalaxyValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace CosmicApi.Application.Features.Galaxies.CreateGalaxy
+{
+    public class DeleteGalaxyValidator : AbstractValidator<DeleteGalaxyRequest>
+    {
+        public DeleteGalaxyValidator()
+        {
+            RuleFor(x => x.Id)
+                .NotEmpty();
+        }
+    }
+}
