018c87d18a2e2013d717bccfea70675214aadb8f
mohammed0xff <mohammed0xff@gamil.com>
2023-03-03 21:32:06 +0200

    add StarResponse DTO

---
+++ src/CosmicApi.Application/Features/Stars/StarResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace CosmicApi.Application.Features.Stars
+{
+    public class StarResponse 
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int NumberOfPlanets { get; set; }
+
+    }
+}
