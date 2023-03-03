432fb91e900aa67d3560949526bfbfbac5418365
mohammed0xff <mohammed0xff@gamil.com>
2023-03-03 22:04:53 +0200

    add `PictureResponse` DTO

---
+++ src/CosmicApi.Application/Features/Pictures/PictureResponse.cs
@@ -0,0 +1,16 @@
+﻿using CosmicApi.Domain.Entities.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace CosmicApi.Application.Features.Pictures
+{
+    public class PictureResponse
+    {
+        public int? Height { get; set; }
+        public int? Width { get; set; }
+
+        [Url]
+        public string URL { get; set; }
+        public DateTime? AddedAt { get; set; } = DateTime.Now;
+
+    }
+}
