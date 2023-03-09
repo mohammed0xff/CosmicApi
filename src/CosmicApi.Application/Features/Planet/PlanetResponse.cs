7ec786f9c6de872606e5acb5148e98b826afd926
mohammed0xff <mohammed0xff@gamil.com>
2023-03-10 00:05:41 +0200

    fix : missed id in `PlanetResponse`

--- src/CosmicApi.Application/Features/Planet/PlanetResponse.cs
+++ src/CosmicApi.Application/Features/Planet/PlanetResponse.cs
@@ -9,6 +9,7 @@ namespace CosmicApi.Application.Features.Planets
 {
     public class PlanetResponse
     {
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public string? Description { get; set; }
         public int? NumberOfMoons { get; set; } = null;
