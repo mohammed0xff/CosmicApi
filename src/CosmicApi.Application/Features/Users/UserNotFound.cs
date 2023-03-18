f8fbe418bb8bbf8d8b89cdf629440ba7c1d1b333
mohammed0xff <mohammed0xff@gamil.com>
2023-03-18 23:42:46 +0200

    add `UserNotFoundException` providing `UserId`

--- src/CosmicApi.Application/Features/Users/UserNotFound.cs
+++ src/CosmicApi.Application/Features/Users/UserNotFound.cs
@@ -1,13 +1,10 @@
-﻿using CosmicApi.Application.Common.Responses;
-
-namespace CosmicApi.Application.Features.Users;
-
-public record UserNotFound : NotFound {}
-
-public class UserNotFoundException : Exception {
-	public UserNotFoundException(Guid userId) 
-		: base($"User with id '{userId}' not found.")
+﻿namespace CosmicApi.Application.Features.Users
+{
+	public class UserNotFoundException : Exception
 	{
+		public UserNotFoundException(Guid userId)
+			: base($"User with id '{userId}' was not found.")
+		{
+		}
 	}
-
 }
