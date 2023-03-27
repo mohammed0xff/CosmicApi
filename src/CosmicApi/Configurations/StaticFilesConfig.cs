278e1a8a50929e5126700f7671cc133bba7d8dff
mohammed0xff <mohammed0xff@gmail.com>
2023-03-27 18:03:19 +0200

    add static files configuration

---
+++ src/CosmicApi/Configurations/StaticFilesConfig.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.FileProviders;
+
+namespace CosmicApi.Configurations
+{
+    public static class StaticFilesConfig
+    {
+        public static void  UseStaticFiles(this IApplicationBuilder app, IWebHostEnvironment environment)
+        {
+            var path = Path.Combine(environment.ContentRootPath, "StaticFiles");
+            if (Directory.Exists(path) == false)
+                Directory.CreateDirectory(path);
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(path),
+                RequestPath = ""
+            });
+        }
+    }
+}
