03f5d4021e8833b5f8abdf902c14be6e3abaf8a3
mohammed0xff <mohammed0xff@gmail.com>
2023-03-27 21:02:02 +0200

    add missing `StarId` to `StarResponse`

--- src/CosmicApi.Application/Features/Stars/StarResponse.cs
+++ src/CosmicApi.Application/Features/Stars/StarResponse.cs
@@ -2,8 +2,9 @@
 {
     public class StarResponse 
     {
+        public Guid StarId { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
+        public string Description { get; set; } = string.Empty;
         public int NumberOfPlanets { get; set; }
 
     }
