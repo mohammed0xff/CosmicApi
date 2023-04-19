609858b946c4cdf664d6d93eabc1535acfd3a7d9
mohammed0xff <mohammed0xff@gmail.com>
2023-04-19 21:30:12 +0200

    add consumer role to `Roles`

--- src/CosmicApi.Domain/Constants/Roles.cs
+++ src/CosmicApi.Domain/Constants/Roles.cs
@@ -2,7 +2,8 @@
 {
     public static class Roles
     {
-        public const string User = "User";
         public const string Admin = "Admin";
+        public const string User = "User";
+        public const string Consumer = "Consumer";
     }
 }
